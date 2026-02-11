c1142f1dcad5b2666805b4525eba33c91af76363
Belal Salah <belal.salah259@gmail.com>
2026-02-11 12:49:07 +0200

    add payment migration

--- database/migrations/2026_02_11_100858_create_payments_table.php
+++ database/migrations/2026_02_11_100858_create_payments_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('payments', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('order_id')->constrained()->cascadeOnDelete();
             $table->decimal('amount');
             $table->string('method')->default(PaymentMethodStatus::CASH->value);
             $table->string('status')->default(PaymentStatus::PENDING->value);
