ad21b6270bd22d6110ff2b93a809f594067f1c54
Belal Salah <belal.salah259@gmail.com>
2026-02-11 12:40:38 +0200

    add role table migration

--- database/migrations/2026_02_11_100539_create_roles_table.php
+++ database/migrations/2026_02_11_100539_create_roles_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Enums\RoleStatus;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,7 +14,7 @@ return new class extends Migration
     {
         Schema::create('roles', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->default(RoleStatus::CUSTOMER->value);
             $table->timestamps();
         });
     }
