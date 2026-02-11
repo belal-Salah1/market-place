85e4ad0df4fab8777bff8c605bb3f8adeb4395ad
Belal Salah <belal.salah259@gmail.com>
2026-02-11 12:47:23 +0200

    add order migration

--- database/migrations/2026_02_11_100807_create_orders_table.php
+++ database/migrations/2026_02_11_100807_create_orders_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('customer_id')->constrained('users')->cascadeOnDelete();
             $table->decimal('total_price');
             $table->string('status')->default(OrderStatus::PENDING->value);
             $table->timestamps();
