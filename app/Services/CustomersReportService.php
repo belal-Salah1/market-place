ae1b0c24e9930fda9fe767496826dd228e360963
Belal Salah <belal.salah259@gmail.com>
2026-02-15 15:26:58 +0200

    adding customers report

---
+++ app/Services/CustomersReportService.php
@@ -0,0 +1,54 @@
+<?php
+
+use Illuminate\Support\Facades\DB;
+
+class CustomersReportService{
+
+public function totalCountOfCustomers(){
+    $totalCountOfCustomers = DB::table('users')->count();
+    return $totalCountOfCustomers;
+}
+
+public function totalCountOfCustmersByRole(){
+    $totalCountOfCustmersByRole = DB::table('users')->join('roles','users.role_id','=','roles.id')->
+    select('roles.name', DB::raw('count(*) as total'))
+    ->groupBy('roles.name')
+    ->get();
+    return $totalCountOfCustmersByRole;
+}
+
+public function topPerformanceVendors(){
+    $topPerformanceVendors = DB::table('users')
+    ->join('orders','users.id','=','orders.user_id')
+    ->join('roles','users.role_id','=','roles.id')
+    ->where('roles.name', 'vendor')
+    ->select('users.name', DB::raw('sum(orders.total_price) as total'))
+    ->groupBy('users.name')
+    ->orderByDesc('total')
+    ->get();
+    return $topPerformanceVendors;
+}
+public function topPerformanceCustomers(){
+    $topPerformanceCustomers = DB::table('users')
+    ->join('orders','users.id','=','orders.user_id')
+    ->join('roles','users.role_id','=','roles.id')
+    ->where('roles.name', 'customer')
+    ->select('users.name', DB::raw('sum(orders.total_price) as total'))
+    ->groupBy('users.name')
+    ->orderByDesc('total')
+    ->get();
+    return $topPerformanceCustomers;
+}
+
+public function topPerformanceAtAll(){
+    $topPerformanceVendors = DB::table('users')
+        ->join('orders','users.id','=','orders.user_id')
+        ->join('roles','users.role_id','=','roles.id')
+        ->select('users.id', 'users.name', DB::raw('SUM(orders.total_price) as total'))
+        ->groupBy('users.id', 'users.name')
+        ->orderByDesc('total')
+        ->get();
+
+    return $topPerformanceVendors;
+}
+}
